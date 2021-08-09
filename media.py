9bf2a1ed57a4a30ee0b901ca8bca7b00e59deff3
Murillo425 <murillo.lourencosilva@outlook.com>
2021-08-09 12:06:32 -0300

    Create media.py

---
+++ media.py
@@ -0,0 +1,16 @@
+# git clone https://github.com/Murillo425/mediaaritmedica.git
+#git :(master) git add.
+#git :(master) git commit - m "media"
+
+Primeiranota= float(input("adicione uma nota em Primeira Nota: "))
+Segundanota= float(input("adicione uma nota em Segunda Nota: "))
+Terceiranota= float(input("adicione uma nota em Terceira Nota: "))
+Quartanota= float(input("adicione uma nota em Quarta Nota:" ))
+
+somadasnotas= (Primeiranota + Segundanota + Terceiranota + Quartanota)
+
+mediaaritmedica= somadasnotas/4
+
+print ("a media aritmedica é ", mediaaritmedica)
+
+#git : (master) git push
